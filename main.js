9ff877163ae9572fdf42ad1d3390b5a74e0ff020
Danny Gleckler <daniel.gleckler@desire2learn.com>
2014-10-10 21:44:42 +0000

    Added link colors

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-CoreStyle.g.theme = {
+CoreStyle.g.theme = _globalTheme = {
 	colors: {
 		palette: ['#7A2E4A','#2A3950','#704E44'],
 		get text() {
@@ -7,6 +7,14 @@ CoreStyle.g.theme = {
 				dim: '#666',
 				dimDim: '#777'
 			}
-		}		
+		},
+		links: {
+			get main() {
+				return _globalTheme.colors.palette[1]
+			},
+			get hover() {
+				return _globalTheme.colors.palette[2]
+			}
+		}
 	}
 };
