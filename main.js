b7402d626fb509388ef5e6cba6a1c9b404ca1e72
Danny Gleckler <daniel.gleckler@desire2learn.com>
2014-10-10 16:33:34 +0000

    Created a theme object

---
+++ main.js
@@ -0,0 +1,12 @@
+CoreStyle.g.theme = {
+	colors: {
+		palette: ['#7A2E4A','#2A3950','#704E44'],
+		get text() {
+			return {
+				main: '#333',
+				dim: '#666',
+				dimDim: '#777'
+			}
+		}		
+	}
+};
